fc273712908926edb97ccc3f95680eddb13f04ed
Ivan Bolshakov <62011032+Ivan-Bolshakov@users.noreply.github.com>
2023-06-29 15:47:19 +0300

    add main func

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,27 @@
 
 int main()
 {
-    std::cout << "version was started ";
+    double a, b, c;
+
+    while (true)
+    {
+        std::cout << "Введите коэффициенты: ";
+        std::cin >> a >> b >> c;
+
+        std::vector<double> roots = QuardraticEquation::solve(a, b, c);
+        if (roots.empty())
+        {
+            std::cout << "Корней нет" << std::endl;
+        }
+        else if (roots.size() == 1)
+        {
+            std::cout << "1 корень: " << roots[0] << std::endl;
+        }
+        else
+        {
+            std::cout << "2 корня: " << roots[0] << " и " << roots[1] << std::endl;
+        }
+    }
 
     return 0;
 }
